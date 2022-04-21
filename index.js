43d2e8eb8f69648d472cdc9daf43d49af94d33fd
Bartłomiej Strama <kontakt@bstrama.com>
2022-04-21 21:58:08 +0200

    origin

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ https://bstrama.com
 var turn = "x",
     numberTurn = true,
     field = [];
+const columns = 5,
+      rows = 5;
 
 function onLoad(){
-  /* mode() */
+  // mode()
   let papiez = Math.floor(Math.random() * 2137 + 1);
   console.log(papiez);
 }
@@ -18,13 +20,14 @@ function mainFunction(field0, field1) {
   console.log(parseInt(`${field0}${field1}`), sid)
   sid.setAttribute('onclick', '')
   field[[field0, field1]] = numberTurn;
+  console.log(field[[field0, field1]])
   sid.innerHTML = `<p>${turn}</p>`;
   numberTurn = !numberTurn;
   turn = numberTurn ? "x" : "o";
-  document.getElementById("now").innerHTML = "Tura " + turn;
-  /*winValidation();*/
+  document.getElementById("now").innerHTML = "Tura: " + turn;
+  rowAndColumnValidation();
 }
-/*
+
 function winValidation() {
   let set1 = new Set([
     rowAndColumnValidation(0,1,2,9, 3),
@@ -58,32 +61,15 @@ function endAndStart(rmOnClickOrAdd){
   }
 }
 
-function rowAndColumnValidation(id1, id2, id3, times, increment) {
-  for (let i = 0; i < times; i+= increment) {
-    if ([
-        field[id1 + i],
-        field[id2 + i],
-        field[id3 + i]].every(n => n === true)) {
-      return "1";
-    } else if ([
-        field[id1 + i],
-        field[id2 + i],
-        field[id3 + i]].every(n => n === false)) {
-      return "0";
+function rowAndColumnValidation(rowOrColumn) {
+  for (let i0 = 0; i0 < columns; i0++){
+    for (let i1 = 0; i1 < rows - 2; i1++){
+      let fieldSet = [ field[[i0, i1]], field[[i0 + rowOrColumn, i1 + rowOrColumn]], field[[i0 + 2, i1 +2]] ]
+      if(fieldSet.indexOf(1) + 1){
+        return [true].concat(fieldSet);
+      }else if(fieldSet.indexOf(0) + 1){
+        return [false].concat(fieldSet);
+      }
     }
   }
 }
-
-function mode() {
-  let value = document.getElementById("mode").checked
-  if(value) {
-    document.body.style.backgroundColor = "rgba(0,0,0,0.64)"
-    document.getElementById("main-div").style.backgroundColor = "#c7c7c7"
-    document.getElementById("mode-label").innerHTML = "Default mode: "
-  } else{
-    document.body.style.backgroundColor = "#cfc"
-    document.getElementById("main-div").style.backgroundColor = "rgba(77, 252, 252, 0.178)"
-    document.getElementById("mode-label").innerHTML = "Dark mode: "
-  }
-}
-*/
